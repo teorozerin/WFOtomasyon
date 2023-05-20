ef118abea37c469cfc9abf89501afd967653fd00
Rozerin Teomete <rozeriinteomete@gmail.com>
2023-05-21 00:43:30 +0300

    Add project files.

---
+++ WFOtomasyon/FRMMain.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace WFOtomasyon
+{
+    partial class FRMMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            dataGridView1 = new DataGridView();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            silToolStripMenuItem = new ToolStripMenuItem();
+            düzenleToolStripMenuItem = new ToolStripMenuItem();
+            groupBox1 = new GroupBox();
+            btnBrand = new Button();
+            txtBrandName = new TextBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            contextMenuStrip1.SuspendLayout();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.ContextMenuStrip = contextMenuStrip1;
+            dataGridView1.Location = new Point(60, 39);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(240, 150);
+            dataGridView1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { silToolStripMenuItem, düzenleToolStripMenuItem });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(181, 70);
+            contextMenuStrip1.Opening += contextMenuStrip1_Opening;
+            // 
+            // silToolStripMenuItem
+            // 
+            silToolStripMenuItem.Name = "silToolStripMenuItem";
+            silToolStripMenuItem.Size = new Size(180, 22);
+            silToolStripMenuItem.Text = "Sil";
+            silToolStripMenuItem.Click += silToolStripMenuItem_Click;
+            // 
+            // düzenleToolStripMenuItem
+            // 
+            düzenleToolStripMenuItem.Name = "düzenleToolStripMenuItem";
+            düzenleToolStripMenuItem.Size = new Size(180, 22);
+            düzenleToolStripMenuItem.Text = "Düzenle";
+            düzenleToolStripMenuItem.Click += düzenleToolStripMenuItem_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnBrand);
+            groupBox1.Controls.Add(txtBrandName);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(389, 39);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(322, 150);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Marka Yönetimi";
+            // 
+            // btnBrand
+            // 
+            btnBrand.Location = new Point(100, 90);
+            btnBrand.Name = "btnBrand";
+            btnBrand.Size = new Size(100, 23);
+            btnBrand.TabIndex = 2;
+            btnBrand.Text = "Marka Ekle";
+            btnBrand.UseVisualStyleBackColor = true;
+            btnBrand.Click += btnBrand_Click;
+            // 
+            // txtBrandName
+            // 
+            txtBrandName.Location = new Point(100, 41);
+            txtBrandName.Name = "txtBrandName";
+            txtBrandName.Size = new Size(100, 23);
+            txtBrandName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(15, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Marka Adı:";
+            // 
+            // FRMMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            Controls.Add(dataGridView1);
+            Name = "FRMMain";
+            Text = "FRMMain";
+            Load += FRMMain_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            contextMenuStrip1.ResumeLayout(false);
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private GroupBox groupBox1;
+        private Button btnBrand;
+        private TextBox txtBrandName;
+        private Label label1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem silToolStripMenuItem;
+        private ToolStripMenuItem düzenleToolStripMenuItem;
+    }
+}
---
+++ WFOtomasyon/FRMMain.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFOtomasyon
+{
+    public partial class FRMMain : Form
+    {
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-F23C5M8;Initial Catalog=OtomasyonDB;Trusted_Connection=True;MultipleActiveResultSets=true;Encrypt=False");
+        public FRMMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FRMMain_Load(object sender, EventArgs e)
+        {
+
+            GridYukle();
+
+        }
+        void GridYukle()
+        {
+            try
+            {
+                baglanti.Open();
+                SqlCommand komut = new SqlCommand("Select * from Brand ", baglanti);
+                DataTable dataTable = new();
+                dataTable.Load(komut.ExecuteReader());
+                dataGridView1.DataSource = dataTable;
+                dataGridView1.Columns["ID"].Visible = false;
+                dataGridView1.Columns["Name"].HeaderText = "Marka Adı";
+
+                //MessageBox.Show("Bağlantı kuruldu...");
+                baglanti.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bağlantı kurulamadı , açıklaması: " + ex.Message);
+            }
+        }
+
+        private void btnBrand_Click(object sender, EventArgs e)
+        {
+            if (btnBrand.Text == "MARKA EKLE")
+            {
+                //SqlCommand komut = new SqlCommand("insert Brand values('"+txtBrandName.Text+"')",baglanti);//SQL Injection
+                SqlCommand komut = new SqlCommand("insert Brand values(@Name)", baglanti);
+                komut.Parameters.AddWithValue("@Name", txtBrandName.Text);
+                baglanti.Open();
+                komut.ExecuteNonQuery();
+                baglanti.Close();
+                GridYukle();
+                txtBrandName.Clear();
+            }
+            else
+            {
+                SqlCommand komut = new SqlCommand("Update Brand set Name=@Name Where id=@id", baglanti);
+                komut.Parameters.AddWithValue("@Name", txtBrandName.Text);
+                int seciliID = Convert.ToInt32(dataGridView1.CurrentRow.Cells["id"].Value);
+                komut.Parameters.AddWithValue("@id", seciliID);
+                baglanti.Open();
+                komut.ExecuteNonQuery();
+                baglanti.Close();
+                GridYukle() ;
+                btnBrand.Text = "MARKA EKLE";
+
+            }
+        }
+         
+        
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void silToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+             
+             DialogResult sonuc = MessageBox.Show("Silmek istediğinizden emin misiniz?",
+                "DİİKKAT!....", MessageBoxButtons.YesNo);
+            if (sonuc == DialogResult.Yes) {
+                SqlCommand komut = new SqlCommand("Delete Brand Where id=@id", baglanti);
+                komut.Parameters.AddWithValue("@Name", txtBrandName.Text);
+                int seciliID = Convert.ToInt32(dataGridView1.CurrentRow.Cells["id"].Value);
+                komut.Parameters.AddWithValue("@id", seciliID);
+                baglanti.Open();
+                komut.ExecuteNonQuery();
+                baglanti.Close();
+                GridYukle();
+                btnBrand.Text = "MARKA EKLE";
+            }
+        }
+
+        private void düzenleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int seciliID = Convert.ToInt32(dataGridView1.CurrentRow.Cells["id"].Value);
+            SqlCommand komut= new("select * from Brand Where id=@id",baglanti);
+            komut.Parameters.AddWithValue("@id",seciliID);
+            baglanti.Open();
+            SqlDataReader okuyucu = komut.ExecuteReader();
+            if(okuyucu.Read()) 
+            {
+                txtBrandName.Text = okuyucu["Name"].ToString();
+                btnBrand.Text = "MARKA GÜNCELLE";
+            }
+            baglanti.Close() ;
+        }
+    }
+}
